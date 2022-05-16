6ad8a1571110a4f827ed2d7be6b719b8b0b16e1d
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2022-05-17 05:22:53 +0900

    [add] 問題3

--- check.sh
+++ check.sh
@@ -23,6 +23,28 @@ check002() {
     fi
 }
 
+check003() {
+    local SHA1
+    SHA1="ae8d4eb1f95026da9916eec542d3fb09ec363c5f"
+    local R
+    echo "問題3 確認中"
+    if [ -f 003/fuga ]; then
+        echo "fugaが残っています、移動されていません"
+        return 1
+    fi
+    if [ ! -f 003/dest/fuga ]; then
+        echo "destディレクトリに移動していません"
+        return 1
+    fi
+
+    R=$(sha1sum < 003/dest/fuga | awk '{print $1}')
+    if [ ! "${R}" = "${SHA1}" ]; then
+        echo "移動したファイルが元のものと異なっております"
+        return 1
+    fi
+    return 0
+}
+
 R=NG
 check001 && R=OK
 echo "問題1 ${R}"
@@ -30,3 +52,7 @@ echo "問題1 ${R}"
 R=NG
 check002 && R=OK
 echo "問題2 ${R}"
+
+R=NG
+check003 && R=OK
+echo "問題3 ${R}"
