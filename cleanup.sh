86b2be2878ed809d8e700a4fde109c6cb0613c1f
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2022-05-19 05:53:17 +0900

    [fix] cleanupを問題ディレクトリで実行する時にデータが残る問題に対応

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+cd $(dirname $0)
 rm -fr 001 002 003 004 005 006 007 008 009 010
 git reset --hard HEAD
+
+EDIR=$(readlink -f . | sed "s;^${HOME};~;")
+cat <<EOM
+問題を初期状態に戻しました。
+もし問題のディレクトリにいた場合は、以下の操作で入り直して下さい。
+
+$ cd ${EDIR}
+$ cd 問題番号のディレクトリ
+EOM
+
